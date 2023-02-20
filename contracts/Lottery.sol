1d657904eedae7c7a00a55b5232900c5965c7233
harshhvv <76173453+harshhvv@users.noreply.github.com>
2023-02-20 12:23:46 +0530

    open zeppeling ownable added

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -2,19 +2,32 @@
 pragma solidity >=0.6.6 <0.9.0;
 
 import "@chainlink/contracts/src/v0.7/interfaces/AggregatorV3Interface.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract Lottery {
+contract Lottery is Ownable {
     address payable[] public players; //array of payable addresses; all players of the lottery
     uint256 public usdEntryFee;
     AggregatorV3Interface internal ethUsdPriceFeed;
+    enum LOTTERY_STATE {
+        OPEN,
+        CLOSED,
+        CALCULATING_WINNER
+    }
+    LOTTERY_STATE public lottery_state;
 
     constructor(address _priceFeedAddress) public {
         usdEntryFee = 50 * (10**18); //50 usd in terms of wei
         ethUsdPriceFeed = AggregatorV3Interface(_priceFeedAddress);
+        lottery_state = LOTTERY_STATE.CLOSED;
     }
 
     function enter() public payable {
         // players.push(msg.sender); //push address of player p to players array
+        require(lottery_state == LOTTERY_STATE.OPEN);
+        require(
+            msg.value >= getEntranceFee(),
+            "You need to spend more eth to enter the lottery."
+        );
         players.push(payable(msg.sender));
     }
 
@@ -25,7 +38,13 @@ contract Lottery {
         return costToEnter;
     }
 
-    function startLottery() public {}
+    function startLottery() public onlyOwner {
+        require(
+            lottery_state == LOTTERY_STATE.CLOSED,
+            "cant start a new lottery yet"
+        );
+        lottery_state = LOTTERY_STATE.OPEN;
+    }
 
-    function endLOttery() public {}
+    function endLOttery() public onlyOwner {}
 }
--- tests/test_lottery.py
+++ tests/test_lottery.py
@@ -8,11 +8,12 @@ def test_get_entrance_fee():
         "eth_usd_price_feed_address"
     ]
     lottery = Lottery.deploy(
-        "0xD4A33860578DE61DBABDC8BFDB98FD742FA7028E", {"from": account}
+        price_feed_address,
+        {"from": account},
     )
 
-    expected_entrance_fee = Web3.toWei(0.025, "ether")
-    print(expected_entrance_fee)
-    print(lottery)
-    ok = lottery.getEntranceFee()
-    assert ok < expected_entrance_fee
+    # expected_entrance_fee = Web3.toWei(0.025, "ether")
+    entranceFee = lottery.getEntranceFee()
+    print(entranceFee)
+    assert entranceFee > Web3.toWei(0.00056, "ether")
+    assert entranceFee < Web3.toWei(0.00060, "ether")
